f7ef8d3e401233be4689197d31ccffda5b615866
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-10-14 11:38:12 -0400

    add nodemon, code monothread  and show var env

--- conceptos/monohilo.js
+++ conceptos/monohilo.js
@@ -2,6 +2,15 @@ console.log(
     "Hola mundo desde el archivo monohilo.js"
 ); 
 
+let i = 0;
 setInterval(function () {
-    console.log("Aun sigo activo")
+    console.log(i);
+    i++;
+
+    //if (i === 5) {
+        //var a = 3 + z;
+    //} --> Hay que ser siempre cuidadosos con el codigo que mandamos a ejecutar en Node
+    
 }, 1000);
+
+console.log('Segunda instruccion');
